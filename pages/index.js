2fc1d611d85113b134d9da47c6da1e92e1b1d165
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-12-21 22:06:18 +0100

    format

--- pages/index.js
+++ pages/index.js
@@ -16,21 +16,23 @@ export default function Home() {
       </Head>
       <main>
         <div className="w-1/3">
-        <div>
-      
-      <div className="mt-1 relative rounded-md shadow-sm">
-        <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-          <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-        </div>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          className="focus:ring-indigo-500 bg-gray-200 ml-2 focus:border-indigo-500 p-2 block w-full pl-10 sm:text-sm border-gray-300 rounded-md"
-          placeholder="Search or start new chat"
-        />
-      </div>
-    </div>
+          <div>
+            <div className="mt-1 relative rounded-md shadow-sm">
+              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                <MagnifyingGlassIcon
+                  className="h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              </div>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="focus:ring-indigo-500 bg-gray-200 ml-2 focus:border-indigo-500 p-2 block w-full pl-10 sm:text-sm border-gray-300 rounded-md"
+                placeholder="Search or start new chat"
+              />
+            </div>
+          </div>
         </div>
 
         <div className="w-2/3"></div>
