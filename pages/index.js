b23b03dd1bed1391219d19aec2cb59e9468119ea
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-12-21 22:05:26 +0100

    remove label

--- pages/index.js
+++ pages/index.js
@@ -17,9 +17,7 @@ export default function Home() {
       <main>
         <div className="w-1/3">
         <div>
-      <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-        Email
-      </label>
+      
       <div className="mt-1 relative rounded-md shadow-sm">
         <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
           <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
@@ -28,7 +26,7 @@ export default function Home() {
           type="email"
           name="email"
           id="email"
-          className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-10 sm:text-sm border-gray-300 rounded-md"
+          className="focus:ring-indigo-500 bg-gray-200 ml-2 focus:border-indigo-500 p-2 block w-full pl-10 sm:text-sm border-gray-300 rounded-md"
           placeholder="Search or start new chat"
         />
       </div>
