bdab3a9a85f640a4793cfc61914def7cfe5e9df6
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-12-22 13:20:11 +0100

    change key

--- components/Profiles.js
+++ components/Profiles.js
@@ -3,84 +3,84 @@ const people = [
   {
     name: "Azzuri",
     email: "lesliealexander@example.com",
-    role: "Co-Founder / CEO",
+    message: "Co-Founder / CEO",
     imageUrl:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
   {
     name: "Chioma Olabode",
     email: "lesliealexander@example.com",
-    role: "Co-Founder / CEO",
+    message: "Co-Founder / CEO",
     imageUrl:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
   {
     name: "Teriekarie2",
     email: "lesliealexander@example.com",
-    role: "Co-Founder / CEO",
+    message: "Co-Founder / CEO",
     imageUrl:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
   {
     name: "Knight Kennel",
     email: "lesliealexander@example.com",
-    role: "Co-Founder / CEO",
+    message: "Co-Founder / CEO",
     imageUrl:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
   {
     name: "Esther Opebi",
     email: "lesliealexander@example.com",
-    role: "Co-Founder / CEO",
+    message: "Co-Founder / CEO",
     imageUrl:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
   {
     name: "Daniel Anthony",
     email: "lesliealexander@example.com",
-    role: "Co-Founder / CEO",
+    message: "Co-Founder / CEO",
     imageUrl:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
   {
     name: "Faith",
     email: "lesliealexander@example.com",
-    role: "Co-Founder / CEO",
+    message: "Co-Founder / CEO",
     imageUrl:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
   {
     name: "Segun Csw",
     email: "lesliealexander@example.com",
-    role: "Co-Founder / CEO",
+    message: "Co-Founder / CEO",
     imageUrl:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
   {
     name: "Olumide",
     email: "lesliealexander@example.com",
-    role: "Co-Founder / CEO",
+    message: "Co-Founder / CEO",
     imageUrl:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
   {
     name: "Db",
     email: "lesliealexander@example.com",
-    role: "Co-Founder / CEO",
+    message: "Co-Founder / CEO",
     imageUrl:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
   {
     name: "Josh",
     email: "lesliealexander@example.com",
-    role: "Co-Founder / CEO",
+    message: "Co-Founder / CEO",
     imageUrl:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
   {
     name: "Dorcas",
     email: "lesliealexander@example.com",
-    role: "Co-Founder / CEO",
+    message: "Co-Founder / CEO",
     imageUrl:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
@@ -112,7 +112,7 @@ export default function Profiles() {
               <p className="text-sm font-medium text-gray-900">{person.name}</p>
               <div className='inline-flex items-baseline'>
                 <CheckIcon className='w-4 h-4'/>
-                <p className="text-sm text-gray-500 truncate">{person.role}</p>
+                <p className="text-sm text-gray-500 truncate">{person.message}</p>
               </div>
             </a>
           </div>
