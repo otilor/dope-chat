fc977513c8126b4bd7d82e8ee4935bc52b5c8108
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-12-22 12:39:49 +0100

    add name

--- components/Profiles.js
+++ components/Profiles.js
@@ -15,7 +15,7 @@ const people = [
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
     },
     {
-        name: 'Leslie Alexander',
+        name: 'Teriekarie2',
       email: 'lesliealexander@example.com',
       role: 'Co-Founder / CEO',
       imageUrl:
