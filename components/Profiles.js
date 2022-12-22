9dbd9ee7877b65ad9aa3db592754fb0963678189
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-12-22 13:03:18 +0100

    remove border radius

--- components/Profiles.js
+++ components/Profiles.js
@@ -30,7 +30,7 @@ export default function Profiles() {
       {people.map((person) => (
         <div
           key={person.email}
-          className="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+          className="relative border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
         >
           <div className="flex-shrink-0">
             <img
