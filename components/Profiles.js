7fc7a03933b08f104ba60b8ca4ca7682fba30d18
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-12-26 16:28:01 +0100

    add margin flow

--- components/Profiles.js
+++ components/Profiles.js
@@ -97,7 +97,7 @@ export default function Profiles() {
       {people.map((person) => (
         <div
           key={person.email}
-          className="relative border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+          className="relative border border-gray-300 md:w-max bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
         >
           <div className="flex-shrink-0">
             <img
